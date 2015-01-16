ca046fe986f360179d3c05fd963668cd98014c50
Nina <ninazeng@umich.edu>
2015-01-15 20:31:26 -0500

    jump

--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,35 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public float speed = 10f;
-	private Animator m_Animator;
+	public float JumpSpeed = 10f ;
+	public LayerMask GroundLayers;
 
+	private Animator m_Animator;
+	private Transform m_GroundCheck;
 	// Use this for initialization
 	void Start () {
 		m_Animator = GetComponent<Animator>();
+		m_GroundCheck = transform.FindChild ("GroundCheck");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		float hSpeed = Input.GetAxis ("Horizontal");
 		m_Animator.SetFloat ("Speed", Mathf.Abs (hSpeed));
-
+		bool isGrounded = Physics2D.OverlapPoint (m_GroundCheck.position, GroundLayers);
+		
+		if (Input.GetButton ("Jump")) 
+		{
+			print (m_GroundCheck.position);
+			print("Wow");
+			if(isGrounded){
+				print("Jumping!");
+				rigidbody2D.AddForce (Vector2.up * JumpSpeed, ForceMode2D.Impulse);
+				isGrounded = false;
+			}			
+		}
+		m_Animator.SetBool("isGrounded", isGrounded);
+		
 		if (hSpeed > 0) {
 			transform.localScale = new Vector3 (.75f, .75f, .75f);
 		} 
