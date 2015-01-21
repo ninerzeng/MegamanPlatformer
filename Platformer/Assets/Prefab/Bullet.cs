fdb6a79150cd15b2a1e759eaa125b13d647cb72e
yunhanwei <yunhanw@umich.edu>
2015-01-20 23:15:19 -0500

    enemy gets hit on the head now

--- Platformer/Assets/Prefab/Bullet.cs
+++ Platformer/Assets/Prefab/Bullet.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 	public float damage = 5;
 	private float start_time = 0;
+	private float bullet_used = 0;
 	// Update is called once per frame
 	void Start () {
 		start_time = Time.time;
 		}
 	void Update () {
 		if ((Time.time - start_time) > 0.5f) {
-						print ("flying too long");
+						//print ("flying too long");
 						//delete bullet
 						int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
 						if (ObjIndex != -1) {
-								print ("DESTROY");
+								//print ("DESTROY");
 								PhysEngine.objs.RemoveAt (ObjIndex);	
 								Destroy (this.gameObject);
 						}
@@ -23,16 +24,20 @@ public class Bullet : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		print ("i hit something");
-			if (other.gameObject.tag == "Enemy") {
-				//subtract health from it
+		//print (other.name);
+			if (other.gameObject.tag == "Enemy" && bullet_used == 0) {
+						//subtract health from it
 				
+				} else {
+						//delete bullet regardless of what is hit
+						int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
+						if (ObjIndex != -1) {
+								print ("DESTROY");
+								PhysEngine.objs.RemoveAt (ObjIndex);	
+								Destroy (this.gameObject);
+						}
 				}
-		//delete bullet regardless of what is hit
-		int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
-		if (ObjIndex != -1) {
-						print ("DESTROY");
-						PhysEngine.objs.RemoveAt (ObjIndex);	
-						Destroy (this.gameObject);
-				}
+		//bullet_used = 1;
 		}
+
 }
