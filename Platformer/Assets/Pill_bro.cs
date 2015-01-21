3c99d5dfb0493d50fde4972a357688da68996648
yunhanwei <yunhanw@umich.edu>
2015-01-20 22:36:28 -0500

    bullet disappears after a certain amount of time, enemy script for pill bug

--- Platformer/Assets/Pill_bro.cs
+++ Platformer/Assets/Pill_bro.cs
@@ -1,15 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Pill_bro : MonoBehaviour {
+	private PE_Obj peo;
+	public float health = 10;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-	
+		peo = GetComponent<PE_Obj>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//if player is to the left, move left
+		if (player.transform.position.x < this.transform.position.x) {
+			peo.vel.x = -2f;
+			}
+		//if player is to the right, move right
+		else if(player.transform.position.x > this.transform.position.x) {
+			peo.vel.x = 2f;
+			}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Bullet") {
+			//take dmg
+			//health = health - other.gameObject.GetComponent<Bullet>().Damage;
+			health = health - 5;
+			if(health <= 0){
+				int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
+				if (ObjIndex != -1) {
+					print ("DESTROY");
+					PhysEngine.objs.RemoveAt (ObjIndex);	
+					Destroy (this.gameObject);
+				}
+				}
+			} 
+		else if (other.gameObject.tag == "Player") {
+			//keep up what youre doing buddy
+			} 
+		else {
+			peo.vel.x = -peo.vel.x;
+			}
 	}
 }
--- Platformer/Assets/Prefab/Bullet.cs
+++ Platformer/Assets/Prefab/Bullet.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
-
+	public float damage = 5;
 	private float start_time = 0;
 	// Update is called once per frame
 	void Start () {
 		start_time = Time.time;
 		}
 	void Update () {
-		if ((Time.time - start_time) > 2f) {
-			print ("flying too long");
-			//delete bullet
+		if ((Time.time - start_time) > 0.5f) {
+						print ("flying too long");
+						//delete bullet
+						int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
+						if (ObjIndex != -1) {
+								print ("DESTROY");
+								PhysEngine.objs.RemoveAt (ObjIndex);	
+								Destroy (this.gameObject);
+						}
 				}
 	}
 
@@ -19,7 +25,14 @@ public class Bullet : MonoBehaviour {
 		print ("i hit something");
 			if (other.gameObject.tag == "Enemy") {
 				//subtract health from it
+				
 				}
-			//delete bullet regardless of what is hit
+		//delete bullet regardless of what is hit
+		int ObjIndex = PhysEngine.objs.IndexOf (this.GetComponent<PE_Obj> () as PE_Obj);
+		if (ObjIndex != -1) {
+						print ("DESTROY");
+						PhysEngine.objs.RemoveAt (ObjIndex);	
+						Destroy (this.gameObject);
+				}
 		}
 }
