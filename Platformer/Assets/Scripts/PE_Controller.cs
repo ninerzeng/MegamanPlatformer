5ba2d7d5529799717e1e870d0727f2e151ab50fd
Nina <ninazeng@umich.edu>
2015-01-24 23:33:58 -0500

    big changes to how collisions are resolved, how stuns are handled, beginning climb framework, animations, map level buildout, death framework

--- Platformer/Assets/Scripts/PE_Controller.cs
+++ Platformer/Assets/Scripts/PE_Controller.cs
@@ -10,15 +10,16 @@ public class PE_Controller : MonoBehaviour {
 	public float	acceleration = 10;
 	public float	jumpVel = 10f;
 	public float	airSteeringAmt = 0.5f;
-
+	public float	ladderSpeed = 1;
 	public float	airMomentumX = 1f; // 0 for no momentum (i.e. 100% drag), 1 for total momentum
 	public float	groundMomentumX = 0.1f;
 
 	public Vector2	maxSpeed = new Vector2( 10, 30 ); // Different x & y to limit maximum falling velocity
-
+	private Animator animator;
 	// Use this for initialization
 	void Start () {
 		peo = GetComponent<PE_Obj>();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -26,27 +27,87 @@ public class PE_Controller : MonoBehaviour {
 	void Update () {
 		vel = peo.vel; // Pull velocity from the PE_Obj
 		grounded = (peo.ground != null);
-		
+		animator.SetBool("grounded",grounded);
 		// Horizontal movement
+		UpdateRun ();
+		UpdateJump ();
+		UpdateClimb ();
+
+		peo.vel = vel;
+	}
+	void UpdateClimb()
+	{
+		if(peo.isClimbing )
+		{
+			//this is up
+			if(Input.GetAxis("Vertical") > 0)
+			{
+				//turn off gravity
+				//move up the ladder at a slow pace
+				//did that for testing
+//				this.transform.Translate(0,20 * Time.deltaTime,0);
+				peo.grav = PE_GravType.none;
+				vel.y = ladderSpeed;
+				SendMessage("onClimb",SendMessageOptions.DontRequireReceiver);
+				animator.SetBool("climbing", true);
+				animator.SetBool("climbIdle", false);
+				
+			}
+			//this is down
+			else if(Input.GetAxis("Vertical") < 0)
+			{
+				vel.y = -ladderSpeed;
+				peo.grav = PE_GravType.none;
+				
+//				this.transform.Translate(0,-10 * Time.deltaTime,0);
+				animator.SetBool("climbing", true);
+				animator.SetBool("climbIdle", false);
+				
+				SendMessage("onClimb",SendMessageOptions.DontRequireReceiver);
+			}
+			else
+			{
+				//if you are not moving then just go idle on the ladder
+				
+				animator.SetBool("climbIdle", true);
+				animator.SetBool("climbing", false);
+				
+				SendMessage("onClimbIdle",SendMessageOptions.DontRequireReceiver);
+			}
+		}
+		else
+		{
+				animator.SetBool("climbing", false);
+				animator.SetBool("climbIdle", false);	
+		}
+	}
+	void UpdateRun()
+	{
 		float vX = Input.GetAxis("Horizontal"); // Returns a number [-1..1]
 		vel.x = vX * hSpeed;
-
-//		if (vX == 0) { // No Horizontal input from keyboard
-//		}
-//		Vector3 accMult = Vector3.one;
-//		if (!grounded) accMult.x = airSteeringAmt;
-//
-//		vel.x += vX * accMult.x * acceleration * Time.deltaTime;
-
+		
+		//animator actions
+		animator.SetFloat ("speed", Mathf.Abs (vel.x));
+		//left right facing
+		if (vel.x > 0) 
+		{
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else if (vel.x < 0) 
+		{
+			transform.localScale = new Vector3 (-1f, 1f, 1f);		
+		}
+	}
+	void UpdateJump()
+	{
 		// Jumping with A (which is x or .)
-		if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Period)) {
+		if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Period)) 
+		{
 			// Jump if you're grounded
-			if (grounded) {
+			if (grounded) 
+			{
 				vel.y = jumpVel;
 				peo.ground = null; // Jumping will set ground = null
 			}
 		}
-
-		peo.vel = vel;
 	}
 }
