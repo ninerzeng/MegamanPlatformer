75e87bb277ce1eb5ef4ef82faa4b0b0910d88df5
yunhanwei <yunhanw@umich.edu>
2015-01-25 19:28:57 -0500

    updated physics engine

--- Platformer/Assets/Scripts/PE_Obj.cs
+++ Platformer/Assets/Scripts/PE_Obj.cs
@@ -13,32 +13,56 @@ public class PE_Obj : MonoBehaviour {
 	public PE_GravType	grav = PE_GravType.constant;
 	
 	public Vector3		acc = Vector3.zero;
-
+	
 	public Vector3		vel = Vector3.zero;
 	public Vector3		vel0 = Vector3.zero;
+	public Vector3		velRel = Vector3.zero;
+	
+	public Vector3		_pos0 = Vector3.zero;
+	public Vector3		_pos1 = Vector3.zero;
 
-	public Vector3		pos0 = Vector3.zero;
-	public Vector3		pos1 = Vector3.zero;
 	public bool			isClimbing = false;
 	public PE_Dir		dir = PE_Dir.still;
-
+	
 	public PE_Obj		ground = null; // Stores whether this is on the ground
-
+	
+	public Vector3		pos0 {
+		get { return( _pos0); }
+		set {
+			float d = (value - _pos0).magnitude;
+			if (d > 1 && gameObject.name == "PC") {
+				Debug.Log ("Big change in pos0!");
+			}
+			_pos0 = value;
+		}
+	}
+	
+	public Vector3		pos1 {
+		get { return( _pos1); }
+		set {
+			float d = (value - _pos1).magnitude;
+			if (d > 1 && gameObject.name == "PC") {
+				Debug.Log ("Big change in pos1!");
+			}
+			_pos1 = value;
+		}
+	}
 
 	void Start() {
 		if (PhysEngine.objs.IndexOf(this) == -1) {
+			_pos1 = _pos0 = transform.position;
 			PhysEngine.objs.Add(this);
 		}
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-	
-	
+		
+		
 	}
-
-
+	
+	
 	void OnTriggerEnter(Collider other) {
 		if(this.tag == "Player" && other.gameObject.tag == "Ladder")
 		{
@@ -53,73 +77,73 @@ public class PE_Obj : MonoBehaviour {
 				playerLoc.x = ladderX;
 				this.gameObject.transform.position = playerLoc;
 			}
-
-			return;
-
-		}
-				//No collision if enemy
-				if (other.tag == "Enemy" && this.tag == "Player") {
-						PlayerHealth m_PlayerHealth = this.GetComponent<PlayerHealth> ();
-						if (m_PlayerHealth.playerHealth <=0 || stunned == true)
-								return;
-
-						if (stunned == false) {
-								PE_Obj nowPE_Obj = this.GetComponent<PE_Obj> ();
-								if(m_PlayerHealth.invulnerable ==false)
-									m_PlayerHealth.playerHealth-=10;
-								if (m_PlayerHealth.playerHealth <=0) {
-										Vector3 zeroVector = new Vector3 (0f, 0f, 0f);
-										nowPE_Obj.vel = zeroVector;
-										Animator animator = this.GetComponent<Animator> ();
-										print ("You died!");
-										animator.SetBool("death", true);
-										PE_Controller m_pController = this.GetComponent<PE_Controller>();
-										m_pController.enabled = false; 
-										return;
-								} else {
-										Vector3 stunVel = nowPE_Obj.vel;
-										Vector3 directionOfStun = this.transform.position - other.transform.position; 
-										directionOfStun.Normalize ();
-//				print (directionOfStun);
-										//stun normally if player is on same level or above the 
-										if (directionOfStun.x >= 0) {
-												stunVel.x = stunStrength;
-												//					stunVel.y = stunStrength;
-										} else {
-												//					stunVel.y = stunStrength;
-												stunVel.x = -1 * stunStrength;
-					
-										}
-				
-										PE_Controller myController = this.GetComponent<PE_Controller> ();
-				
-										myController.enabled = false;
-										nowPE_Obj.vel = stunVel;
-										StartCoroutine (bounceDelay (myController));
-
-								}
-
 			
-								return;
-						}
+			return;
+			
+		}
+		//No collision if enemy
+		if (other.tag == "Enemy" && this.tag == "Player") {
+			PlayerHealth m_PlayerHealth = this.GetComponent<PlayerHealth> ();
+			if (m_PlayerHealth.playerHealth <=0 || stunned == true)
+				return;
+			
+			if (stunned == false) {
+				PE_Obj nowPE_Obj = this.GetComponent<PE_Obj> ();
+				if(m_PlayerHealth.invulnerable ==false)
+					m_PlayerHealth.playerHealth-=10;
+				if (m_PlayerHealth.playerHealth <=0) {
+					Vector3 zeroVector = new Vector3 (0f, 0f, 0f);
+					nowPE_Obj.vel = zeroVector;
+					Animator animator = this.GetComponent<Animator> ();
+					print ("You died!");
+					animator.SetBool("death", true);
+					PE_Controller m_pController = this.GetComponent<PE_Controller>();
+					m_pController.enabled = false; 
+					return;
+				} else {
+					Vector3 stunVel = nowPE_Obj.vel;
+					Vector3 directionOfStun = this.transform.position - other.transform.position; 
+					directionOfStun.Normalize ();
+					//				print (directionOfStun);
+					//stun normally if player is on same level or above the 
+					if (directionOfStun.x >= 0) {
+						stunVel.x = stunStrength;
+						//					stunVel.y = stunStrength;
+					} else {
+						//					stunVel.y = stunStrength;
+						stunVel.x = -1 * stunStrength;
+						
+					}
+					
+					PE_Controller myController = this.GetComponent<PE_Controller> ();
+					
+					myController.enabled = false;
+					nowPE_Obj.vel = stunVel;
+					StartCoroutine (bounceDelay (myController));
+					
 				}
-	
+				
+				
+				return;
+			}
+		}
+		
 		if (this.tag == "Enemy" && other.gameObject.tag == "Player") {
 			return;		
 		}
 		// Ignore collisions of still objects
 		if (still) return;
-
+		
 		PE_Obj otherPEO = other.GetComponent<PE_Obj>();
 		if (otherPEO == null) return;
-
+		
 		ResolveCollisionWith(otherPEO);
 	}
-
+	
 	void OnTriggerStay(Collider other) {
 		OnTriggerEnter(other);
 	}
-
+	
 	void OnTriggerxit(Collider other) {
 		if(other.gameObject.tag == "Ladder" && this.tag == "Player")
 		{
@@ -131,7 +155,7 @@ public class PE_Obj : MonoBehaviour {
 		
 		PE_Obj otherPEO = other.GetComponent<PE_Obj>();
 		if (otherPEO == null) return;
-
+		
 		// This sets ground to null if we fall off of the current ground
 		// Jumping will also set ground to null
 		if (ground == otherPEO) {
@@ -139,13 +163,27 @@ public class PE_Obj : MonoBehaviour {
 		}
 	}
 
+	void OnDrawGizmos() {
+		if (vel.magnitude != 0) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(a0,0.2f);
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(a0,a1);
+			Gizmos.DrawWireSphere(a1,0.2f);
+			Gizmos.color = Color.green;
+			Gizmos.DrawLine(a1,posFinal);
+			Gizmos.DrawWireSphere(posFinal,0.2f);
+		}
+	}
+	public Vector3 a0, a1, b, delta, pU, posFinal; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
+
 	void ResolveCollisionWith(PE_Obj that) {
 		// Assumes that "that" is still
 		Vector3 posFinal = pos1; // Sets a defaut value for posFinal
-
+		
 		switch (this.coll) {
 		case PE_Collider.sphere:
-
+			
 			switch (that.coll) {
 			case PE_Collider.sphere:
 				// Sphere / Sphere collision
@@ -154,61 +192,63 @@ public class PE_Obj : MonoBehaviour {
 				thisR = Mathf.Max( this.transform.lossyScale.x, this.transform.lossyScale.y, this.transform.lossyScale.z ) / 2;
 				thatR = Mathf.Max( that.transform.lossyScale.x, that.transform.lossyScale.y, that.transform.lossyScale.z ) / 2;
 				rad = thisR + thatR;
-
+				
 				Vector3 delta = pos1 - that.transform.position;
 				delta.Normalize();
 				delta *= rad;
-
+				
 				posFinal = that.transform.position + delta;
 				break;
 			}
-
+			
 			break;
-
+			
 		case PE_Collider.aabb:
-
+			
 			switch (that.coll) {
 			case PE_Collider.aabb:
 				// AABB / AABB collision
 				// Axis-Aligned Bounding Box
 				// With AABB collisions, we're usually concerned with corners and deciding which corner to consider when making comparisons.
 				// I believe that this corner should be determined by looking at the velocity of the moving body (this one)
-
-				Vector3 a0, a1, b; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
+				
+				//Vector3 a0, a1, b; // a0-moving corner last frame, a1-moving corner now, b-comparison corner on other object
 				a0 = a1 = b = Vector3.zero;	 // Sets a default value to keep the compiler from complaining
-				Vector3 delta = pos1 - pos0;
-
+			    delta = pos1 - pos0;
+				
 				if (dir == PE_Dir.down) {
-					// Just resolve to be on top
+					// If a0 was above b and a1 is below b resolve to be on top
 					a1 = pos1;
 					a1.y -= transform.lossyScale.y/2f;
+					a0 = a1 - delta;
 					b = that.pos1;
 					b.y += that.transform.lossyScale.y/2f;
-					if (b.y > a1.y) {
+					if ( PhysEngine.GEQ( a0.y, b.y ) && b.y > a1.y) {
 						posFinal.y += Mathf.Abs( a1.y - b.y );
+						// Handle vel
+						vel.y = 0;
+						
+						if (ground == null) ground = that;
 					}
-					// Handle vel
-					vel.y = 0;
-
-					if (ground == null) ground = that;
 					break; // Exit this switch statement: switch (that.coll)
 				}
-
+				
 				if (dir == PE_Dir.up) {
-					// Just resolve to be below
+					// If a0 was below b and a1 is above b resolve to be below
 					a1 = pos1;
 					a1.y += transform.lossyScale.y/2f;
+					a0 = a1 - delta;
 					b = that.pos1;
 					b.y -= that.transform.lossyScale.y/2f;
-					if (b.y < a1.y) {
+					if ( PhysEngine.LEQ( a0.y, b.y ) && b.y < a1.y) {
 						posFinal.y -= Mathf.Abs( a1.y - b.y );
+						// Handle vel
+						vel.y = 0;
+						
+						break; // Exit this switch statement: switch (that.coll)
 					}
-					// Handle vel
-					vel.y = 0;
-
-					break; // Exit this switch statement: switch (that.coll)
 				}
-
+				
 				if (dir == PE_Dir.upRight) { // Bottom, Left is the comparison corner
 					a1 = pos1;
 					a1.x += (transform.lossyScale.x/2f+0.1f);
@@ -218,7 +258,7 @@ public class PE_Obj : MonoBehaviour {
 					b.x -= that.transform.lossyScale.x/2f;
 					b.y -= that.transform.localScale.y/2f;
 				}
-
+				
 				if (dir == PE_Dir.upLeft) { // Bottom, Right is the comparison corner
 					a1 = pos1;
 					a1.x -= (transform.lossyScale.x/2f+0.1f);
@@ -228,9 +268,9 @@ public class PE_Obj : MonoBehaviour {
 					b.x += that.transform.lossyScale.x/2f;
 					b.y -= that.transform.localScale.y/2f;
 				}
-
+				
 				if (dir == PE_Dir.downLeft) { // Top, Right is the comparison corner
- 					a1 = pos1;
+					a1 = pos1;
 					a1.x -= (transform.lossyScale.x/2f+0.1f);
 					a1.y -= transform.lossyScale.y/2f;
 					a0 = a1 - delta;
@@ -238,7 +278,7 @@ public class PE_Obj : MonoBehaviour {
 					b.x += that.transform.lossyScale.x/2f;
 					b.y += that.transform.localScale.y/2f;
 				}
-
+				
 				if (dir == PE_Dir.downRight) { // Top, Left is the comparison corner
 					a1 = pos1;
 					a1.x += (transform.lossyScale.x/2f+0.1f);
@@ -248,41 +288,47 @@ public class PE_Obj : MonoBehaviour {
 					b.x -= that.transform.lossyScale.x/2f;
 					b.y += that.transform.localScale.y/2f;
 				}
-
+				
 				// In the x dimension, find how far along the line segment between a0 and a1 we need to go to encounter b
 				float u = (b.x - a0.x) / (a1.x - a0.x);
-
+				
 				// Determine this point using linear interpolation (see the appendix of the book)
-				Vector3 pU = (1-u)*a0 + u*a1;
+				pU = (1-u)*a0 + u*a1;
 
+				// Find distance we would have to offset in x or y
+				float offsetX = Mathf.Abs(a1.x - b.x);
+				float offsetY = Mathf.Abs(a1.y - b.y);
+				bool usedX = false;
+				
 				// Use pU.y vs. b.y to tell which side of PE_Obj "that" PE_Obj "this" should be on
 				switch (dir) {
 				case PE_Dir.upRight:
 					if (pU.y > b.y) { // hit the left side
 						posFinal.x -= Mathf.Abs(a1.x - b.x);
-
+						
 						// Handle vel
 						vel.x = 0;
-
+						usedX = true;
 					} else { // hit the bottom
 						posFinal.y -= Mathf.Abs(a1.y - b.y);
 						
 						// Handle vel
 						vel.y = 0;
-
+						
 					}
 					break;
-
+					
 				case PE_Dir.downRight:
 					if (pU.y < b.y) { // hit the left side
 						posFinal.x -= Mathf.Abs(a1.x - b.x);
 						
 						// Handle vel
 						vel.x = 0;
-
+						usedX = true;
+						
 					} else { // hit the top
 						posFinal.y += Mathf.Abs(a1.y - b.y);
-
+						
 						// Handle vel
 						vel.y = 0;
 						
@@ -296,13 +342,13 @@ public class PE_Obj : MonoBehaviour {
 						
 						// Handle vel
 						vel.x = 0;
-
+						usedX = true;
 					} else { // hit the bottom
 						posFinal.y -= Mathf.Abs(a1.y - b.y);
 						
 						// Handle vel
 						vel.y = 0;
-
+						
 					}
 					break;
 					
@@ -312,7 +358,7 @@ public class PE_Obj : MonoBehaviour {
 						
 						// Handle vel
 						vel.x = 0;
-
+						usedX = true;
 					} else { // hit the top
 						posFinal.y += Mathf.Abs(b.y - a1.y);
 						
@@ -323,28 +369,28 @@ public class PE_Obj : MonoBehaviour {
 					}
 					break;
 				}
-
+				
 				break;
 			}
-
+			
 			break;
 		}
-
+		
 		transform.position = pos1 = posFinal;
 	}
-	IEnumerator bounceDelay(PE_Controller myController) {
-			stunned = true;
-			Animator animator = this.GetComponent<Animator>();
-			
-			animator.SetBool("stunned", true);
-			yield return new WaitForSeconds(stunDelay);
-			myController.enabled = true;
-		//to prevent stunlocking
-			yield return new WaitForSeconds (.1f);
-			animator.SetBool("stunned", false);
-			
-			
-			stunned = false;
-		}
 
+	IEnumerator bounceDelay(PE_Controller myController) {
+		stunned = true;
+		Animator animator = this.GetComponent<Animator>();
+		
+		animator.SetBool("stunned", true);
+		yield return new WaitForSeconds(stunDelay);
+		myController.enabled = true;
+		//to prevent stunlocking
+		yield return new WaitForSeconds (.1f);
+		animator.SetBool("stunned", false);
+		
+		
+		stunned = false;
+	}
 }
--- Platformer/Assets/Scripts/PhysEngine.cs
+++ Platformer/Assets/Scripts/PhysEngine.cs
@@ -25,44 +25,45 @@ public enum PE_Dir { // The direction in which the PE_Obj is moving
 
 public class PhysEngine : MonoBehaviour {
 	static public List<PE_Obj>	objs;
-
+	static public float			closeEnough = 0.1f;
+	
 	public Vector3		gravity = new Vector3(0,-9.8f,0);
-
+	
 	// Use this for initialization
 	void Awake() {
 		objs = new List<PE_Obj>();
 	}
-
-
+	
+	
 	void FixedUpdate () {
 		// Handle the timestep for each object
 		float dt = Time.fixedDeltaTime;
 		foreach (PE_Obj po in objs) {
 			TimeStep(po, dt);
 		}
-
+		
 		// Resolve collisions
-
-
+		// In a normal physics engine, this is where collisions would be resolved.
+		// But in Unity, because of the way we're using triggers to sense collisions, this step happens in OnCollisionEnter() and OnCollisionStay()
+		
+		
 		// Finalize positions
 		foreach (PE_Obj po in objs) {
-			if(po == null)
-				return;
 			po.transform.position = po.pos1;
 		}
-
+		
 	}
-
-
+	
+	
 	public void TimeStep(PE_Obj po, float dt) {
-		if (po == null)
-			return;
-		if (po.still ) {
+		if (po.still) {
 			po.pos0 = po.pos1 = po.transform.position;
 			return;
 		}
-
+		
 		// Velocity
+		po.velRel = (po.pos1 - po.pos0) / dt;
+		
 		po.vel0 = po.vel;
 		Vector3 tAcc = po.acc;
 		switch (po.grav) {
@@ -71,7 +72,7 @@ public class PhysEngine : MonoBehaviour {
 			break;
 		}
 		po.vel += tAcc * dt;
-
+		
 		if (po.vel.x==0) { // Special case when po.vel.x == 0
 			if (po.vel.y > 0) {
 				po.dir = PE_Dir.up;
@@ -87,28 +88,35 @@ public class PhysEngine : MonoBehaviour {
 		} else if (po.vel.x<0 && po.vel.y>0) {
 			po.dir = PE_Dir.upLeft;
 		}
-
+		
 		// Position
-		po.pos1 = po.pos0 = po.transform.position;
+		po.pos0 = po.transform.position;
 		po.pos1 += po.vel * dt;
-
+		
 	}
-//	public void destroyObject(GameObject toBeDestroyed)
-//	{
-//		print ("Physics!");
-//		int ObjIndex = this.objs.IndexOf (toBeDestroyed.GetComponent<PE_Obj>() as PE_Obj);
-//		if (ObjIndex!= -1) {
-//			this.objs.RemoveAt(ObjIndex);
-//		}
-		//		foreach (PE_Obj po in objs) {
-//			if(po.Equals(toBeDestroyed))
-//			{
-//				print ("DESTROY");
-//				objs.Remove(po);
-//				Destroy (toBeDestroyed,.2f);
-//				return;
-//			}
-//		}
 	
-//	}
+	
+	// Static equality functions to deal with floating point math errors
+	static public bool EQ(float f0, float f1) {
+		if ( Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+	
+	static public bool LEQ(float f0, float f1) {
+		if ( f0 < f1 || Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+	
+	static public bool GEQ(float f0, float f1) {
+		if ( f0 > f1 || Mathf.Abs(f1-f0) <= closeEnough ) {
+			return( true );
+		}
+		return( false );
+	}
+	
+	
 }
